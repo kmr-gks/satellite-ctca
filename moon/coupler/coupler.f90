e1a90ab3ea0d98d1a08feebe2df5690cb66dca9c
kmr-gks <82916941+kmr-gks@users.noreply.github.com>
2024-11-27 22:10:00 +0900

    Updated coupler, worker with comments

--- moon/coupler/coupler.f90
+++ moon/coupler/coupler.f90
@@ -1,29 +1,30 @@
-program coupler_test
+program coupler
   use mpi
   use ctca
   implicit none
 !
   integer(kind=4) :: ierr, myrank, nprocs
+  !エリアID
   integer(kind=4) :: phi_areaid
   integer(kind=4) :: reqinf(4)
-  integer(kind=4) :: frmrank, progid, ndat=10
-  integer(kind=4) :: datint(10)
+  integer(kind=4) :: frmrank, progid
+  integer(kind=4) :: req_params(10)
 !
   call CTCAC_init()
   call MPI_Comm_size(CTCA_subcomm, nprocs, ierr)
   call MPI_Comm_rank(CTCA_subcomm, myrank, ierr)
 !
-! 領域確保 ハンドルを取得する感じ
+! エリアIDを取得
   call CTCAC_regarea_real8(phi_areaid)
 !
   do while (.true.)
-    call CTCAC_pollreq(reqinf,frmrank,datint,ndat)
+    call CTCAC_pollreq(reqinf,frmrank,req_params,size(req_params))
     if( CTCAC_isfin() ) exit
     progid = 0
-    call CTCAC_enqreq(reqinf,progid,datint,ndat)
+    call CTCAC_enqreq(reqinf,progid,req_params,size(req_params))
   end do
 !
   call CTCAC_finalize()
 !
   stop
-end program coupler_test
+end program coupler
--- moon/worker/worker.f90
+++ moon/worker/worker.f90
@@ -1,53 +1,52 @@
-program dout
+program worker
   use mpi
   use ctca
   implicit none
 !
-  type pinfo
-    integer(kind=4) :: rank, pgrid(2), offset(2), dsize(2), stat
-  end type
-  integer(kind=4) :: ierr, myrank, nprocs, len
-  integer(kind=4) :: phi_areaid,phi_area_size=35
-  integer(kind=4) :: frmrank, hdat(10), ndat=10
-  integer(kind=4) :: nsdom, lsdom(2)
-  integer(kind=4) :: wflag(10) = 0
-  integer(kind=4) :: nread, iread, jread
-  integer(kind=4) :: szx, szy, szt, osx, osy
-  real(kind=4),allocatable :: odat(:,:), sdat(:,:)
-  type(pinfo),allocatable :: ptable(:)
-  character(128) :: hostname
-  character(7) :: ofname
-  logical :: vb=.false.
-  integer(kind=4) :: i, j, k, m
-  integer :: socket, ret, comlen
+  integer :: ierr, myrank, nprocs
+  !エリアID
+  integer :: phi_areaid
+  integer :: i
+  integer :: from_rank
+  !リクエストを受け取るときのデータ
+  integer ::req_params(10)
+  !受け取るデータとそのサイズ
   real*8,allocatable    :: read_data(:)
+  integer :: read_data_size
 !
   call CTCAW_init(0, 1)
   call MPI_Comm_size(CTCA_subcomm, nprocs, ierr)
   call MPI_Comm_rank(CTCA_subcomm, myrank, ierr)
-  call MPI_Get_processor_name(hostname, len, ierr)
 !
-  print*, "dout: ", myrank, " / ", nprocs, " : ", hostname
+  print*, "worker: ", myrank, " / ", nprocs
 !
+! エリアIDを取得
   call CTCAW_regarea_real8(phi_areaid)
-
 !
-  allocate(read_data(phi_area_size))
   do while( .true. )
-    call CTCAW_pollreq(frmrank,hdat,ndat)
+    !リクエストを受けとる
+    call CTCAW_pollreq(from_rank,req_params,size(req_params))
     if( CTCAW_isfin() ) exit
-    !read_areaしてポテンシャルを取得する
-    call CTCAW_readarea_real8(phi_areaid,51,0,phi_area_size,read_data)
-    print*, "CTCAworker: read_data="!, read_data
-    do i = 1, size(read_data)
+    !リクエスト時のデータをもとに受け取るデータの情報をみる
+    from_rank = req_params(1)
+    read_data_size = req_params(2)
+    !初回のみ領域確保
+    if(.not.allocated(read_data)) then
+      allocate(read_data(read_data_size))
+    end if
+
+    !read_dataにデータを読み込む
+    call CTCAW_readarea_real8(phi_areaid,from_rank,0,read_data_size,read_data)
+    print*, "CTCAworker: read_data="
+    do i = 1, read_data_size
       write(*, "(A,E)", advance="no") ",", read_data(i)
     end do
 
     call CTCAW_complete()
   end do
 
-  print*, "dout is finalizing..."
+  print*, "worker is finalizing..."
   call CTCAW_finalize()
 !
   stop
-end program dout
+end program worker
