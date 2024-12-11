1fa2d2752be879cf56837f6e3007e5f34f85b323
kmr-gks <82916941+kmr-gks@users.noreply.github.com>
2024-12-11 18:18:41 +0900

    Added memo, Updated job script

--- moon/exp_surface/job.sh
+++ moon/exp_surface/job.sh
@@ -26,10 +26,4 @@ date
 
 echo ...done
 
-python generate_xdmf3.py nd*.h5 rhobk00_0000.h5
-python generate_xdmf3.py rho00_0000.h5
-python generate_xdmf3.py phisp00_0000.h5
-python generate_xdmf3.py ex00_0000.h5 ey00_0000.h5 ez00_0000.h5
-python generate_xdmf3.py j1x00_0000.h5 j1y00_0000.h5 j1z00_0000.h5 j2x00_0000.h5 j2y00_0000.h5 j2z00_0000.h5 j3x00_0000.h5 j3y00_0000.h5 j3z00_0000.h5
-
 date
