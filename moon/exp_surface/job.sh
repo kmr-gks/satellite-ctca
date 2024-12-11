0af98805d193c6bd4e57984643ceec6e6486fee4
kmr-gks <82916941+kmr-gks@users.noreply.github.com>
2024-12-11 22:11:06 +0900

    Update to set rank from env var and check csv

--- moon/exp_surface/job.sh
+++ moon/exp_surface/job.sh
@@ -7,6 +7,8 @@
 # 標準出力と標準エラー出力をリダイレクト
 exec 2>&1
 
+cat $0
+
 #set -x
 #export LD_LIBRARY_PATH="/LARGE0/gr20001/KUspace-share/common/hdf5-lib/hdf5-1.14.3-240410/lib:/LARGE0/gr20001/KUspace-share/common/fftw-lib/fftw-3.3.10-240410/lib:$LD_LIBRARY_PATH"
 
@@ -19,19 +21,31 @@ export EMSES_DEBUG=no
 export SHIPY=5
 export SHIPZ=145
 export NEIGHBOUR_THR=80
-export OUTPUT_FILE_NAME="output,sy=${SHIPY},sz=${SHIPZ},nt=${NEIGHBOUR_THR}.csv"
+export FROM_RANK=10
+export OUTPUT_FILE_NAME="output,sy=${SHIPY},sz=${SHIPZ},nt=${NEIGHBOUR_THR},fr=${FROM_RANK}.csv"
 
 date
 
 rm *_0000.h5
 #dont use sbatch, use mysbatch and njob.sh instead
-srun -n130 -l --multi-prog multi.conf
+srun -l --multi-prog multi.conf
 date
 
 # Postprocessing(visualization code, etc.)
 
 echo ...done
 
-python histogram.py 
+if [ -f "$OUTPUT_FILE_NAME" ]; then
+	# ファイルサイズを取得 (バイト単位)
+	FILE_SIZE=$(stat -c%s "$OUTPUT_FILE_NAME")
+	if [ "$FILE_SIZE" -ge 1024 ]; then
+		echo "Running python script..."
+		python histogram.py 
+	else
+		echo "Size of file '$OUTPUT_FILE_NAME' is too small."
+	fi
+else
+	echo "File '$OUTPUT_FILE_NAME' does not exist."
+fi
 
 date
