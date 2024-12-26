8809913793607d8314a917e38cc2e3ca4db6486c
kmr-gks <82916941+kmr-gks@users.noreply.github.com>
2024-12-26 11:12:04 +0900

    Updated comment

--- moon/modify/ctcamain.F90
+++ moon/modify/ctcamain.F90
@@ -7,18 +7,20 @@ module m_ctcamain
     implicit none
     private
     public cotocoa_init, cotocoa_mainstep, cotocoa_finalize
-
-    real(kind=8) :: ion_charge=1.60217662d-19,ion_mass=1.6726219d-27,permittivity=8.854187817d-12
+    !elementary charge[C], ion mass[kg], permittivity of vacuum[F/m]
+    real(kind=8) :: ion_charge=1.6021766d-19,ion_mass=1.6726219d-27,permittivity=8.85418781d-12
+    !ratio of emses to real unit, ion density[/cc]
     real(kind=8) :: len_ratio,vel_ratio,time_ratio,freq_ratio,ion_density
 
-    !共有するphiの配列
+    !distance between satellite and particles, energy density
     real(kind=8),allocatable :: dist(:),energy(:)
-    !共有する配列のサイズ
+    !size of pbuf,members of pbuf
     integer(kind=8)       :: pbuf_size, pbuf_mem=6
-    !共有領域のエリアID
+    !area id of pbuf, size of energy
     integer               :: pbuf_id,energy_size,i
     !position of satellite
     real(kind=8) :: shipx,shipy,shipz
+    !neighbour threshold, super particle mass, grid length, neighbour volume
     real(kind=8) :: neighbour_thr,sup_par_mass,grid_length=0.5,neighbour_vol
     !environment variables
     character(len=100) :: env_shipy,env_shipz,env_neighbour_thr
@@ -27,16 +29,16 @@ contains
 
     subroutine cotocoa_init
         implicit none
-        !plasma frequency for ion (real unit)
+        !plasma frequency for ion (emses, real unit)
         real(kind=8) :: wp_ion_emses,wp_ion_real
+        !simulation volume
         real(kind=8) :: simu_vol
+        !number of super particles
         integer sup_par_num
 
         pbuf_size=size(pbuf)
-        !共有する配列の領域を確保
         allocate(dist(pbuf_size))
         allocate(energy(pbuf_size))
-        !エリアIDを取得
         call CTCAR_regarea_real8(energy,pbuf_size,pbuf_id)
 
         ! set parameters from environment variables
@@ -47,19 +49,21 @@ contains
         call get_environment_variable("NEIGHBOUR_THR",env_neighbour_thr)
         read(env_neighbour_thr,*) neighbour_thr
 
-        ! set plasma frequency for ion
+        ! get plasma frequency for ion
         wp_ion_emses=wp(2)
+
         len_ratio=1/grid_length
         vel_ratio=cv*1e3/2.99792458d8
         time_ratio=len_ratio/vel_ratio
         freq_ratio=1/time_ratio
+
         wp_ion_real=wp_ion_emses/freq_ratio
         ion_density=wp_ion_real**2*ion_mass*permittivity/ion_charge**2/1e6
         simu_vol=nx*ny*nz*grid_length**3
-        !super particle number
+
         sup_par_num=nodes(1)*nodes(2)*nodes(3)*pbuf_size
         sup_par_mass=ion_mass*(simu_vol*1d6*ion_density/sup_par_num)
-        neighbour_vol=4/3*3.14159265*(neighbour_thr*grid_length)**3
+        neighbour_vol=4/3*pi*(neighbour_thr*grid_length)**3
         if (myid.eq.0) then
             print *, "wp_ion_emses=",wp_ion_emses
             print *, "wp_ion_real=",wp_ion_real,"[Hz]"
@@ -74,11 +78,14 @@ contains
     subroutine cotocoa_mainstep
         implicit none
 
-        !リクエストを送るときのデータ
+        !data for request
         integer(kind=4) ::req_params(10)
 
+        !set position of satellite
         shipx=istep
+
         dist(:)=sqrt((pbuf(:)%x-shipx)**2+(pbuf(:)%y-shipy)**2+(pbuf(:)%z-shipz)**2)
+        
         energy_size=0
         do i=1, pbuf_size
             if (dist(i).lt.neighbour_thr) then
@@ -88,13 +95,13 @@ contains
             end if
         end do
 
-        !リクエスト時のデータを設定
+        !set request data
         req_params(1)=myid
         req_params(2)=pbuf_size
         req_params(3)=pbuf_mem
         req_params(4)=istep
         req_params(5)=energy_size
-        !リクエスト時にデータを送ることができる
+        !send request
         call CTCAR_sendreq(req_params,size(req_params))
     
     end subroutine cotocoa_mainstep
--- moon/worker/worker.f90
+++ moon/worker/worker.f90
@@ -4,13 +4,12 @@ program worker
   implicit none
 !
   integer :: ierr, myrank, nprocs
-  !エリアID
+  !area id
   integer :: pbuf_id
   integer :: i,step=0
   integer :: from_rank
-  !リクエストを受け取るときのデータ
+  !data for request
   integer ::req_params(10)
-  !受け取るデータとそのサイズ
   real*8,allocatable    :: energy(:)
   integer :: pbuf_size, pbuf_mem, energy_size=0
   real(kind=8) :: satellite_pos, grid_length=0.5
@@ -24,29 +23,29 @@ program worker
 !
   print*, "worker: ", myrank, " / ", nprocs
   call get_environment_variable("OUTPUT_FILE_NAME",output_file_name)
-! エリアIDを取得
+! get area id
   call CTCAW_regarea_real8(pbuf_id)
   !open the output file
   open(unit=output_file_unit,file=output_file_name, status='replace', action='write')
   write(output_file_unit,'(A)') "step,energy"
 !
   do while( .true. )
-    !リクエストを受けとる
+    !polling request
     call CTCAW_pollreq(from_rank,req_params,size(req_params))
     if( CTCAW_isfin() ) exit
-    !リクエスト時のデータをもとに受け取るデータの情報をみる
+    !get information from request
     from_rank = req_params(1)
     pbuf_size = req_params(2)
     pbuf_mem = req_params(3)
     step = req_params(4)
     satellite_pos = step*grid_length
     energy_size = req_params(5)
-    !初回のみ領域確保
+    !allocate energy array
     if(.not.allocated(energy)) then
       allocate(energy(pbuf_size))
     end if
 
-    !read_dataにデータを読み込む
+    !read energy from pbuf
     call CTCAW_readarea_real8(pbuf_id,from_rank,0,energy_size,energy)
     
     do i=1, energy_size
