400ce91f826409cc562d8f2a17bd52f0521283e0
kmr-gks <82916941+kmr-gks@users.noreply.github.com>
2025-01-04 22:38:06 +0900

    Update saving graph of both particles

--- moon/exp_surface/histogram.py
+++ moon/exp_surface/histogram.py
@@ -4,34 +4,28 @@ import pandas as pd
 import numpy as np
 import os
 
-#file_name = os.environ["OUTPUT_FILE_NAME"]
-file_name="output,sy=16,sz=256,nt=10_11.csv"
+def save_hist2d(df, x, y, bins, title, xlabel, ylabel, file_name):
+	plt.clf()
+	plt.hist2d(df[x], df[y], bins=bins, norm=LogNorm())
+	plt.title(title)
+	plt.yscale('log')
+	plt.colorbar(label='number of super particles')
+	plt.xlabel(xlabel)
+	plt.ylabel(ylabel)
+	plt.savefig(file_name, dpi=300)
+
+file_name = os.environ["OUTPUT_FILE_NAME"]
 
 #load data from file
-df_original=pd.read_csv(file_name)
+df=pd.read_csv(file_name)
 
-df=df_original[df_original['species']==1]
-log_bins=[9, np.logspace(np.log10(df['energy'].min()),np.log10(df['energy'].max()), num=50)]
+log_bins=[len(df['step'].value_counts()), np.logspace(np.log10(df['energy'].min()),np.log10(df['energy'].max()), num=50)]
+
 #plot histogram of electron energy
-plt.clf()
-plt.hist2d(df['step'], df['energy'], bins=log_bins, norm=LogNorm())
-title="Energy distribution (electron)"
-plt.title(title)
-plt.yscale('log')
-plt.colorbar(label='number of super particles')
-plt.xlabel("x[m]")
-plt.ylabel("energy density [eV/m^3]")
-plt.savefig(file_name.replace('.csv','')+title, dpi=300)
+save_hist2d(df[df['species']==1], 'step', 'energy', log_bins, "Energy distribution (electron)", "x[m]", "energy density [eV/m^3]", file_name.replace('.csv','')+"Energy distribution (electron)")
 
-df=df_original[df_original['species']==2]
-log_bins=[9, np.logspace(np.log10(df['energy'].min()),np.log10(df['energy'].max()), num=50)]
 #plot histogram of ion energy
-plt.clf()
-plt.hist2d(df['step'], df['energy'], bins=log_bins, norm=LogNorm())
-title="Energy distribution (ion)"
-plt.title(title)
-plt.yscale('log')
-plt.colorbar(label='number of super particles')
-plt.xlabel("x[m]")
-plt.ylabel("energy density [eV/m^3]")
-plt.savefig(file_name.replace('.csv','')+title, dpi=300)
+save_hist2d(df[df['species']==2], 'step', 'energy', log_bins, "Energy distribution (ion)", "x[m]", "energy density [eV/m^3]", file_name.replace('.csv','')+"Energy distribution (ion)")
+
+#plot histogram of all particles energy
+save_hist2d(df, 'step', 'energy', log_bins, "Energy distribution (all particles)", "x[m]", "energy density [eV/m^3]", file_name.replace('.csv','')+"Energy distribution (all particles)")
