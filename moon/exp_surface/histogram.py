eba5d0dac11404027843a67a788f2c82156d9781
kmr-gks <82916941+kmr-gks@users.noreply.github.com>
2025-01-09 11:10:47 +0900

    fixed range of colorbar

--- moon/exp_surface/histogram.py
+++ moon/exp_surface/histogram.py
@@ -10,7 +10,7 @@ def save_hist2d(df, title, file_name):
 	#csv data is binned by time-step and energy, so we can use pivot_table to create a 2D histogram
 	hist_data = df.pivot_table(index='energy(10*log10eV)', columns='time-step', values='sup-par-count', aggfunc=np.sum, fill_value=0)
 	#plot histogram
-	plt.imshow(hist_data, aspect='auto', origin='lower', extent=[time_min, time_max, energy_min, energy_max], norm=LogNorm())
+	plt.imshow(hist_data, aspect='auto', origin='lower', extent=[time_min, time_max, energy_min, energy_max], norm=LogNorm(vmin=count_min, vmax=count_max))
 	plt.colorbar(label=colorbar_label)
 	plt.title(title)
 	plt.xlabel(xlabel)
@@ -27,7 +27,6 @@ output_file_name = file_name.replace('.csv','')+"Energy distribution"
 #find real_par_num_per_sup_par from job output (JOB_OUT_FILE)
 pattern = r'real_par_num_per_sup_par=\s+(\d+)'
 with open(os.environ["JOB_OUT_FILE"], 'r') as f:
-#with open("job.sh.3610830.out", 'r') as f:
 	for line in f:
 		match = re.search(pattern, line)
 		if match:
@@ -36,6 +35,8 @@ with open(os.environ["JOB_OUT_FILE"], 'r') as f:
 
 #load data from file
 df_all=pd.read_csv(file_name)
+#multiply sup-par-count by real_par_num_per_sup_par
+df_all['sup-par-count'] = df_all['sup-par-count'] * real_par_num_per_sup_par
 #get nonzero data
 df_non0=df_all[df_all['sup-par-count'] > 0]
 #get extent of data
@@ -43,10 +44,10 @@ time_min = df_non0['time-step'].min()
 time_max = df_non0['time-step'].max()
 energy_min = df_non0['energy(10*log10eV)'].min()
 energy_max = df_non0['energy(10*log10eV)'].max()
+count_min = df_non0['sup-par-count'].min()
+count_max = df_non0['sup-par-count'].max()
 #filter data to only include the range of interest
 df_all=df_all[(df_all['time-step'] >= time_min) & (df_all['time-step'] <= time_max) & (df_all['energy(10*log10eV)'] >= energy_min) & (df_all['energy(10*log10eV)'] <= energy_max)]
-#multiply sup-par-count by real_par_num_per_sup_par
-df_all['sup-par-count'] = df_all['sup-par-count'] * real_par_num_per_sup_par
 #split data by species
 df_ele=df_all[df_all['species'] == 1]
 df_ion=df_all[df_all['species'] == 2]
