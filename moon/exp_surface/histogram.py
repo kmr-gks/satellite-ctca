7b85ca33b1615d98057f42c7b4cbd8f20035f9f0
kmr-gks <82916941+kmr-gks@users.noreply.github.com>
2025-01-10 20:55:50 +0900

    Align the range of the graph

--- moon/exp_surface/histogram.py
+++ moon/exp_surface/histogram.py
@@ -22,21 +22,30 @@ def save_hist2d(df, column, title):
 	plt.ylabel(ylabel)
 	#set scale for energy(10*log10eV -> linear scale)
 	plt.gca().yaxis.set_major_formatter(FuncFormatter(log_formatter))
-	plt.savefig(output_file_name+title+'.png', dpi=300)
+	plt.savefig(title+'.png', dpi=300)
 
 if 'OUTPUT_FILE_NAME' in os.environ: #called from job script
 	file_name = os.environ["OUTPUT_FILE_NAME"]
 else: #default file name if not called from job script
-	file_name="y16,z128,t10.csv"
+	file_name="output.csv"
 colorbar_label='number of actual particles'
 xlabel='time [sec]'
 ylabel='Energy [eV]'
-output_file_name = file_name.replace('.csv','')
 
 #load data from file
 df_all=pd.read_csv(file_name)
 #get extent of data
 time_min, time_max = df_all['time'].agg(['min', 'max'])
+#get nonzero data
+df_non0 = df_all[(df_all[['par-count', 'vx-count', 'vy-count', 'vz-count']] > 0).any(axis=1)].replace(0, np.nan)
+#get extent of data
+energy_min, energy_max = df_non0['energy(10*log10eV)'].agg(['min', 'max'])
+count_min, count_max = df_non0[['par-count', 'vx-count', 'vy-count', 'vz-count']].min().min(), df_non0[['par-count', 'vx-count', 'vy-count', 'vz-count']].max().max()
+#filter data to only include the range of interest
+df_allpar = df_all[
+	df_all['time'].between(time_min, time_max) &
+	df_all['energy(10*log10eV)'].between(energy_min, energy_max)
+]
 
 columns = ['par-count', 'vx-count', 'vy-count', 'vz-count']
 descriptions = ['Energy', 'x-comp. of vel.', 'y-comp. of vel.', 'z-comp. of vel.']
@@ -45,16 +54,6 @@ ylabels = ['Energy [eV]', 'x-comp. of vel. [m/s]', 'y-comp. of vel. [m/s]', 'z-c
 
 #save histograms
 for column, description,xlabel,ylabel in zip(columns, descriptions,xlabels,ylabels):
-	#get nonzero data
-	df_non0=df_all[df_all[column] > 0]
-	#get extent of data
-	energy_min, energy_max = df_non0['energy(10*log10eV)'].agg(['min', 'max'])
-	count_min, count_max = df_non0[column].agg(['min', 'max'])
-	#filter data to only include the range of interest
-	df_allpar = df_all[
-		df_all['time'].between(time_min, time_max) &
-		df_all['energy(10*log10eV)'].between(energy_min, energy_max)
-	]
 	#split data by species
 	df_ele, df_ion = df_allpar[df_allpar['species'] == 1], df_allpar[df_allpar['species'] == 2]
 	save_hist2d(df_allpar, column, description+"(all particles)")
