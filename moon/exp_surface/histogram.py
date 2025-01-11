0532603136907e330baf8d084ba72d67289a9950
kmr-gks <82916941+kmr-gks@users.noreply.github.com>
2025-01-12 00:48:42 +0900

    onnecting two graphs into one image

--- moon/exp_surface/histogram.py
+++ moon/exp_surface/histogram.py
@@ -10,19 +10,17 @@ import re
 def log_formatter(value, tick_number):
     return f"$10^{{{int(value/10)}}}$"
 
-def save_hist2d(df, column, title):
-	plt.clf()
+def save_hist2d(fig, axe, df, column, title):
 	#csv data is binned by time and energy, so we can use pivot_table to create a 2D histogram
 	hist_data = df.pivot_table(index='energy(10*log10eV)', columns='time', values=column, aggfunc=np.sum, fill_value=0)
 	#plot histogram
-	plt.imshow(hist_data, aspect='auto', origin='lower', extent=[time_min, time_max, energy_min, energy_max], norm=LogNorm(vmin=count_min, vmax=count_max))
-	plt.colorbar(label=colorbar_label)
-	plt.title(title)
-	plt.xlabel(xlabel)
-	plt.ylabel(ylabel)
+	im= axe.imshow(hist_data, aspect='auto', origin='lower', extent=[time_min, time_max, energy_min, energy_max], norm=LogNorm(vmin=count_min, vmax=count_max))
+	fig.colorbar(im, label=colorbar_label)
+	axe.set_title(title)
+	axe.set_xlabel(xlabel)
+	axe.set_ylabel(ylabel)
 	#set scale for energy(10*log10eV -> linear scale)
-	plt.gca().yaxis.set_major_formatter(FuncFormatter(log_formatter))
-	plt.savefig(title+'.png', dpi=300)
+	axe.yaxis.set_major_formatter(FuncFormatter(log_formatter))
 
 if 'OUTPUT_FILE_NAME' in os.environ: #called from job script
 	file_name = os.environ["OUTPUT_FILE_NAME"]
@@ -51,9 +49,13 @@ df_allpar = df_all[
 ]
 #split data by species and save histograms
 df_ele, df_ion = df_allpar[df_allpar['species'] == 1], df_allpar[df_allpar['species'] == 2]
-save_hist2d(df_allpar, column, description+"(all particles)")
-save_hist2d(df_ele, column, description+"(electron)")
-save_hist2d(df_ion, column, description+"(ion)")
+
+#save histograms for energy
+fig, axes = plt.subplots(1, 2, figsize=(14, 6))
+save_hist2d(fig, axes[0], df_ele, column, description+"(electron)")
+save_hist2d(fig, axes[1], df_ion, column, description+"(ion)")
+plt.tight_layout()
+plt.savefig(description+' of all particles.png', dpi=300)
 
 #save histograms for velocity
 columns = ['vx-count', 'vy-count', 'vz-count', 'vx-p', 'vy-p', 'vz-p', 'vx-n', 'vy-n', 'vz-n']
@@ -76,6 +78,8 @@ df_allpar = df_all[
 for column, description,xlabel,ylabel in zip(columns, descriptions,xlabels,ylabels):
 	#split data by species
 	df_ele, df_ion = df_allpar[df_allpar['species'] == 1], df_allpar[df_allpar['species'] == 2]
-	save_hist2d(df_allpar, column, description+"(all particles)")
-	save_hist2d(df_ele, column, description+"(electron)")
-	save_hist2d(df_ion, column, description+"(ion)")
+	fig, axes = plt.subplots(1, 2, figsize=(14, 6))
+	save_hist2d(fig, axes[0], df_ele, column, description+"(electron)")
+	save_hist2d(fig, axes[1], df_ion, column, description+"(ion)")
+	plt.tight_layout()
+	plt.savefig(description+' of all particles.png', dpi=300)
