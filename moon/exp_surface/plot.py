822f9d54ea08ba6ad1f7b89c876d3cf1be3a51b2
kmr-gks <82916941+kmr-gks@users.noreply.github.com>
2025-02-03 12:33:38 +0900

    update plot.py

--- moon/exp_surface/plot.py
+++ moon/exp_surface/plot.py
@@ -2,7 +2,8 @@ import emout
 import matplotlib.pyplot as plt
 import os
 
-job = '3822856'
+#job = '3982757'
+job = '3799126'
 dirname=job+'_plot/'
 
 data = emout.Emout('output/' + job)
@@ -11,9 +12,6 @@ os.makedirs(job + '_plot',exist_ok=True)
 #data.phisp[-1, :, :, 256].val_si.plot()
 print(data.phisp[:].val_si.shape)
 x, y, z = 128, 128, 60
-
-data.phisp[:, z, :, :].val_si.gifplot(action='save', filename=dirname+'phisp_z.gif') # for save on a file
-
 
 data.j1yz[-1, :, :, x].val_si.plot(mode='stream', savefilename=dirname+"j1yz_x.png")
 data.j1xz[-1, :, y, :].val_si.plot(mode='stream', savefilename=dirname+"j1xz_y.png")
@@ -32,11 +30,22 @@ data.phisp[-1, :, :, x].val_si.plot(savefilename=dirname+"phisp_x.png")
 data.phisp[-1, :, y, :].val_si.plot(savefilename=dirname+"phisp_y.png")
 data.phisp[-1, z, :, :].val_si.plot(savefilename=dirname+"phisp_z.png")
 
+data.phisp[:, :, :, x].val_si.gifplot(action='save', filename=dirname+'phisp_x.gif')
+data.phisp[:, :, y, :].val_si.gifplot(action='save', filename=dirname+'phisp_y.gif')
+data.phisp[:, z, :, :].val_si.gifplot(action='save', filename=dirname+'phisp_z.gif')
+
 data.nd1p[-1, :, :, x].val_si.plot(savefilename=dirname+"nd1p_x.png")
 data.nd1p[-1, z, :, :].val_si.plot(savefilename=dirname+"nd1p_z.png")
 data.nd1p[-1, :, y, :].val_si.plot(savefilename=dirname+"nd1p_y.png")
+
+data.nd1p[:, :, :, x].val_si.gifplot(action='save', filename=dirname+'nd1p_x.gif')
+data.nd1p[:, :, y, :].val_si.gifplot(action='save', filename=dirname+'nd1p_y.gif')
+data.nd1p[:, z, :, :].val_si.gifplot(action='save', filename=dirname+'nd1p_z.gif')
 
 data.nd2p[-1, :, :, x].val_si.plot(savefilename=dirname+"nd2p_x.png")
 data.nd2p[-1, :, y, :].val_si.plot(savefilename=dirname+"nd2p_y.png")
 data.nd2p[-1, z, :, :].val_si.plot(savefilename=dirname+"nd2p_z.png")
 
+data.nd2p[:, :, :, x].val_si.gifplot(action='save', filename=dirname+'nd2p_x.gif')
+data.nd2p[:, :, y, :].val_si.gifplot(action='save', filename=dirname+'nd2p_y.gif')
+data.nd2p[:, z, :, :].val_si.gifplot(action='save', filename=dirname+'nd2p_z.gif')
